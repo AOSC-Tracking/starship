dd25fe463af9365096cdd259a43e560eacd84abd
eatradish <sakiiily@aosc.io>
2023-09-21 18:11:16 +0800

    refactor(utils): move `is_root_user` function to `utils` mod

---
+++ src/modules/utils/is_root_user.rs
@@ -0,0 +1,31 @@
+#[cfg(all(target_os = "windows", not(test)))]
+pub fn is_root_user() -> bool {
+    use deelevate::{PrivilegeLevel, Token};
+    let token = match Token::with_current_process() {
+        Ok(token) => token,
+        Err(e) => {
+            log::warn!("Failed to get process token: {e:?}");
+            return false;
+        }
+    };
+    matches!(
+        match token.privilege_level() {
+            Ok(level) => level,
+            Err(e) => {
+                log::warn!("Failed to get privilege level: {e:?}");
+                return false;
+            }
+        },
+        PrivilegeLevel::Elevated | PrivilegeLevel::HighIntegrityAdmin
+    )
+}
+
+#[cfg(test)]
+pub fn is_root_user() -> bool {
+    false
+}
+
+#[cfg(all(not(target_os = "windows"), not(test)))]
+pub fn is_root_user() -> bool {
+    nix::unistd::geteuid() == nix::unistd::ROOT
+}
